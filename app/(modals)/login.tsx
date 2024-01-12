3b8dbcfc71e1f76b5c7959151af3a6f19391b475
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-01-11 23:52:50 -0500

    Add warm up browser hook for android perfomance

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
 
 const Page = () => {
+  useWarmUpBrowser();
+
   return (
     <View>
       <Text>Login</Text>
