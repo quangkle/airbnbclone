3d2a44f3f8b4c01436c7eab44c5eb3157d031ade
Quang Le <150883573+quangkle@users.noreply.github.com>
2024-01-12 23:45:44 -0500

    Add explore header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listings from "@/components/Listings";
 
 const Page = () => {
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
-      <Link href={"/listing/10"}>Listing details</Link>
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader />
+        }}
+      />
+
+      <Listings />
     </View>
   );
 };
