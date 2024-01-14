132a66d27c3bac467a7375584324a0c8e04ff4b5
Quang Le <150883573+quangkle@users.noreply.github.com>
2024-01-14 14:34:18 -0500

    Add map clustering

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,36 +1,68 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import MapView, { Marker, PROVIDER_GOOGLE, Region } from "react-native-maps";
+import { Marker, PROVIDER_GOOGLE, Region } from "react-native-maps";
 import { useRouter } from "expo-router";
+import MapView from "react-native-map-clustering";
 
 interface Props {
   items: any;
 }
 
 const INITIAL_REGION: Region = {
-    latitude: 37.33,
-    longitude: -122,
-    latitudeDelta: 9,
-    longitudeDelta: 9,
-}
+  latitude: 37.33,
+  longitude: -122,
+  latitudeDelta: 9,
+  longitudeDelta: 9,
+};
 
 const ListingsMap = ({ items }: Props) => {
   const router = useRouter();
   const onMarkerSelected = (item: ListingGeo) => {
     router.push(`/listing/${item.properties.id}`);
-  }
+  };
+
+  const renderCluster = (cluster: any) => {
+    const { id, geometry, onPress, properties } = cluster;
+    const points = properties.point_count;
+
+    return (
+      <Marker
+        key={`cluster-${id}`}
+        onPress={onPress}
+        coordinate={{
+          longitude: geometry.coordinates[0],
+          latitude: geometry.coordinates[1],
+        }}
+      >
+        <View style={styles.marker}>
+          <Text style={{
+            color: "#000",
+            textAlign: "center",
+            fontFamily: "mon-sb",
+          }}>
+            {points}
+          </Text>
+        </View>
+      </Marker>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <MapView
+        animationEnabled={false}
         style={StyleSheet.absoluteFill}
         provider={PROVIDER_GOOGLE}
         showsUserLocation
         showsMyLocationButton
         initialRegion={INITIAL_REGION}
+        clusterColor="#fff"
+        clusterTextColor="#000"
+        clusterFontFamily="mon-sb"
+        renderCluster={renderCluster}
       >
         {items.features.map((item: ListingGeo) => (
-          <Marker 
+          <Marker
             key={item.properties.id}
             onPress={() => onMarkerSelected(item)}
             coordinate={{
@@ -65,12 +97,12 @@ const styles = StyleSheet.create({
     shadowOffset: {
       width: 1,
       height: 10,
-    }
+    },
   },
   markerText: {
     fontSize: 14,
     fontFamily: "mon-sb",
-  }
+  },
 });
 
 export default ListingsMap;
