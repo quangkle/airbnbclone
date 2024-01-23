1b2956a971964b15d4b0900454514eac87bccf5a
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-01-22 23:29:27 -0500

    Create profile form

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,19 @@
-import { View, Text, Button, StyleSheet, SafeAreaView } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { useAuth, useUser } from '@clerk/clerk-expo';
-import { Link } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
-import { defaultStyles } from '@/constants/Styles';
-import Colors from '@/constants/Colors';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useAuth, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
@@ -12,7 +21,7 @@ const Page = () => {
   const [firstName, setFirstName] = useState(user?.firstName);
   const [lastName, setLastName] = useState(user?.lastName);
   const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
-  const [edit, setEdit] = useState(false);
+  const [editing, setEditing] = useState(false);
 
   useEffect(() => {
     if (!user) return;
@@ -20,33 +29,84 @@ const Page = () => {
     setFirstName(user.firstName);
     setLastName(user.lastName);
     setEmail(user.emailAddresses[0].emailAddress);
-  } , [user]);
+  }, [user]);
 
   const onSaveUser = async () => {
+    setEditing(false);
+  };
 
-  }
-
-  const onCaptureImage = async () => {
-
-  }
+  const onCaptureImage = async () => {};
 
   return (
     <SafeAreaView style={defaultStyles.container}>
       <View style={styles.headerContainer}>
         <Text style={styles.header}>Profile</Text>
-        <Ionicons name='notifications-outline' size={26} />
+        <Ionicons name="notifications-outline" size={26} />
       </View>
 
-      {isSignedIn && <Button title="Log out" color={Colors.dark} onPress={() => signOut()} />}
+      {user && (
+        <View style={styles.card}>
+          <TouchableOpacity onPress={onCaptureImage}>
+            <Image
+              source={{ uri: user?.imageUrl }}
+              style={styles.avatarImage}
+            />
+          </TouchableOpacity>
+          <View style={{ flexDirection: "row", gap: 6 }}>
+            {editing ? (
+              <View style={styles.editRow}>
+                <TextInput
+                  placeholder="First name"
+                  value={firstName || ""}
+                  onChangeText={setFirstName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TextInput
+                  placeholder="Last name"
+                  value={lastName || ""}
+                  onChangeText={setLastName}
+                  style={[defaultStyles.inputField, { width: 100 }]}
+                />
+                <TouchableOpacity onPress={onSaveUser}>
+                  <Ionicons
+                    name="checkmark-outline"
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View style={styles.editRow}>
+                <Text style={{ fontFamily: "mon-b", fontSize: 22 }}>
+                  {firstName} {lastName}
+                </Text>
+                <TouchableOpacity onPress={() => setEditing(true)}>
+                  <Ionicons
+                    name="create-outline"
+                    size={24}
+                    color={Colors.dark}
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+          <Text>{email}</Text>
+          <Text>Since {user?.createdAt?.toLocaleDateString()}</Text>
+        </View>
+      )}
+
+      {isSignedIn && (
+        <Button title="Log out" color={Colors.dark} onPress={() => signOut()} />
+      )}
 
       {!isSignedIn && (
         <Link href={"/(modals)/login"} asChild>
-          <Text>Login</Text>
+          <Button title="Log In" color={Colors.dark} />
         </Link>
       )}
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   headerContainer: {
@@ -58,7 +118,39 @@ const styles = StyleSheet.create({
   header: {
     fontFamily: "mon-b",
     fontSize: 24,
-  }
-})
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    alignItems: "center",
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatarImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  editRow: {
+    height: 60,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+});
 
-export default Page
+export default Page;
