d65ffb0d53f9236c671b1dd3458756cb028f19fb
Quang Le <150883573+quangkle@users.noreply.github.com>
2024-01-26 23:52:31 -0500

    Add animated cards for booking page

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,31 +1,114 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
 import { BlurView } from "expo-blur";
-import Animated, { SlideInDown } from "react-native-reanimated";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  SlideInDown,
+} from "react-native-reanimated";
 import { defaultStyles } from "@/constants/Styles";
 import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
 const Page = () => {
-  const onClearAll = () => {};
   const [openCard, setOpenCard] = useState(0);
-  const [selectedPlace, setSelectedPlace] = useState(0); 
+  const [selectedPlace, setSelectedPlace] = useState(0);
+
+  const onClearAll = () => {
+    setSelectedPlace(0);
+    setOpenCard(0);
+  };
 
   return (
     <BlurView intensity={70} style={styles.container}>
-      <Text>Bookings</Text>
+      {/* Where */}
+      <View style={styles.card}>
+        {openCard !== 0 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(0)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>Where</Text>
+            <Text style={styles.previewDate}>I'm flexible</Text>
+          </AnimatedTouchableOpacity>
+        )}
 
+        {openCard === 0 && (
+          <Animated.View>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              Where to?
+            </Animated.Text>
+          </Animated.View>
+        )}
+      </View>
+
+      {/* When */}
+      <View style={styles.card}>
+        {openCard !== 1 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(1)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>When</Text>
+            <Text style={styles.previewDate}>Any week</Text>
+          </AnimatedTouchableOpacity>
+        )}
+
+        {openCard === 1 && (
+          <Animated.View>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              When's your trip?
+            </Animated.Text>
+          </Animated.View>
+        )}
+      </View>
+
+      {/* Who  */}
+      <View style={styles.card}>
+        {openCard !== 2 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(2)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>Who</Text>
+            <Text style={styles.previewDate}>Add guests</Text>
+          </AnimatedTouchableOpacity>
+        )}
+
+        {openCard === 2 && (
+          <Animated.View>
+            <Animated.Text entering={FadeIn} style={styles.cardHeader}>
+              Who's coming?
+            </Animated.Text>
+          </Animated.View>
+        )}
+      </View>
+
+      {/* Footer */}
       <Animated.View
         style={defaultStyles.footer}
         entering={SlideInDown.delay(200)}
       >
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <TouchableOpacity onPress={onClearAll} style={{justifyContent: "center"}}>
+          <TouchableOpacity
+            onPress={onClearAll}
+            style={{ justifyContent: "center" }}
+          >
             <Text style={styles.clearAllText}>Clear all</Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => router.back()}
-            style={[defaultStyles.btn, {paddingRight: 20, paddingLeft: 50}]}
+            style={[defaultStyles.btn, { paddingRight: 20, paddingLeft: 50 }]}
           >
             <Ionicons
               name="search-outline"
@@ -50,6 +133,40 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontFamily: "mon-sb",
     textDecorationLine: "underline",
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 14,
+    margin: 10,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    gap: 20,
+  },
+  previewText: {
+    fontFamily: "mon-sb",
+    fontSize: 14,
+    color: Colors.grey,
+  },
+  previewDate: {
+    fontFamily: "mon-sb",
+    fontSize: 14,
+    color: Colors.dark,
+  },
+  cardPreview: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  cardHeader: {
+    fontFamily: "mon-sb",
+    fontSize: 24,
+    padding: 20,
   },
 });
 
